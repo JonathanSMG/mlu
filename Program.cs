b117ddd258fc94cac0ff2f332882b0f4d9f91b51
Jonathan Marin <105480882+JonathanSMG@users.noreply.github.com>
2022-05-14 21:16:46 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ejercicio_manana.Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Empleados empleados = new Empleados("Bar de comida", 2022, "Pepe", "Aguilar", 1, "Soltero");
+            empleados.imprimir();
+            Estudiantes estudiante = new Estudiantes("A340", "Pablo", "Espinalezs", 2, "Soltero");
+            estudiante.imprimir();
+            Personal personal = new Personal("Secretaria", "Diana", "Lopez", 3, "Casada");
+            personal.imprimir();
+            Profesores profesores = new Profesores("Arquitectura", "Joaquin", "Gonzales", 3, "viudo");
+            profesores.imprimir();
+        }
+    }
+    }
